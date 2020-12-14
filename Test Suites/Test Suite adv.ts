88d748174c8ebea9a097d305c042b94a63448f1d
ahmed <ahmed@EBKW-AHANY>
2020-12-14 22:48:55 +0200

    edit test suites

---
+++ Test Suites/Test Suite adv.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite adv</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>800cde7e-68d7-494c-9446-44535118332a</testSuiteGuid>
+   <testCaseLink>
+      <guid>7f4bfc86-672c-4b87-b04f-ecba44f386fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/add adv</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
